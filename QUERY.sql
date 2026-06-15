85eacdc5b97917639d68a32115c32ce5881ea84b
ei-shadi <eftajulislam.saadi@gmail.com>
2026-06-15 22:06:40 +0600

    Completed Query 3

--- QUERY.sql
+++ QUERY.sql
@@ -33,3 +33,18 @@ OR
 
 
 
+-- =========================================================================
+-- QUERY-03: Retrieve all booking records where the payment status is missing (NULL), 
+-- replacing the empty result with 'Action Required'.
+-- =========================================================================
+SELECT
+  booking_id, 
+  user_id, match_id, 
+  COALESCE(payment_status, 'Action Required') AS systematic_status
+FROM 
+  bookings
+WHERE 
+  payment_status IS NULL;
+
+
+
